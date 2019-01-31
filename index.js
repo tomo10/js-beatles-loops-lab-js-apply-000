3577d664825b735a83acfc243ba543ba44b4aeae
Thomas Edwards <thce28@gmail.com>
2019-01-31 14:30:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var arr = [];
-  for (i = 0; i < musicians.length; i++) {
+  for (var i = 0; i < musicians.length; i++) {
     arr.push(musicians[i] + " plays " + instruments[i])
   }
   return arr;
